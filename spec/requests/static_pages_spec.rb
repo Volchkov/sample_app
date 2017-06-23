55081ee22e6ff613c67881441d7ed50a94124a57
Vasil <vasvol@ua.fm>
2017-06-23 16:41:30 +0300

    Add next test - title

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,19 +9,6 @@ describe "Static pages" do
       expect(page).to have_content('Sample App')
     end
   end
-end
-
-require 'spec_helper'
-
-describe "Static pages" do
-
-  describe "Home page" do
-
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
-    end
-  end
 
   describe "Help page" do
 
@@ -30,4 +17,37 @@ describe "Static pages" do
       expect(page).to have_content('Help')
     end
   end
+
+  describe "About page" do
+
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+  end
+  
+  describe "Home page" do
+
+  it "should have the right title" do
+     visit '/static_pages/home'
+     expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+    end 
+  end
+
+  describe "Help page" do
+  	
+  it "should have the right title" do
+     visit '/static_pages/home'
+     expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+    end
+  end   
+
+   describe "About page" do
+  	
+  it "should have the right title" do
+     visit '/static_pages/home'
+     expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
+    end
+   end
+   
 end
