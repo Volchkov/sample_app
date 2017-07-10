36d3631c1c64d599c7d73f5dfbf9de4310b5d7d6
Vasil <vasvol@ua.fm>
2017-07-10 13:14:41 +0300

    Initial commit Test Static page

--- Gemfile
+++ Gemfile
@@ -7,9 +7,9 @@ end
 
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '~> 5.1.1'
+gem 'rails', '~> 5.1.2'
 # Use postgresql as the database for Active Record
-gem 'pg', '~> 0.18'
+gem 'pg'
 # Use Puma as the app server
 gem 'puma', '~> 3.7'
 # Use SCSS for stylesheets
@@ -18,7 +18,7 @@ gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
-
+gem 'spork', '= 0.9.2'
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
@@ -39,8 +39,12 @@ group :development, :test do
   # Adds support for Capybara system testing and selenium driver
   gem 'capybara', '~> 2.13'
   gem 'selenium-webdriver'
-  gem 'rspec-rails'
-  gem 'pg', '~> 0.18'
+  gem 'rspec-rails', '3.6.0'
+  #gem 'pg', '~> 0.18'
+  gem 'guard-spork', '2.1.0'
+  gem 'childprocess', '0.7.1'
+  gem 'guard-rspec', '~> 4.7'
+  #gem 'spork-rails', '4.0.0'
 end
 
 group :development do
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,11 +2,23 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
+    end
+
+    it "should have the base title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
     end
   end
 
@@ -16,6 +28,11 @@ describe "Static pages" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("#{base_title} | Help")
+    end
   end
 
   describe "About page" do
@@ -24,42 +41,23 @@ describe "Static pages" do
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
     end
-  end
-  
-  describe "Home page" do
 
-  it "should have the right title" do
-     visit '/static_pages/home'
-     expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
-    end 
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("#{base_title} | About Us")
+    end
   end
 
-  describe "Help page" do
-  	
-  it "should have the right title" do
-     visit '/static_pages/help'
-     expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
-    end
-  end   
-
-   describe "About page" do
-  	
-  it "should have the right title" do
-     visit '/static_pages/about'
-     expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
-    end
-   end
-
-    describe "Contact" do
+  describe "Contact page" do
 
     it "should have the content 'Contact'" do
       visit '/static_pages/contact'
       expect(page).to have_content('Contact')
     end
 
-    it "should have the title 'contact'" do
+    it "should have the title 'Contact'" do
       visit '/static_pages/contact'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
-    end
+      expect(page).to have_title("#{base_title} | Contact")
     end
   end
+end
